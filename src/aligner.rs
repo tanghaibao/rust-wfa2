22b609daa8e39687513f48b28955ff1a889078eb
Haibao Tang <tanghaibao@gmail.com>
2022-05-08 23:59:52 -0700

    test aligning

--- src/aligner.rs
+++ src/aligner.rs
@@ -1,4 +1,5 @@
 use crate::wfa2;
+use std::slice;
 
 enum MemoryModel {
     MemoryHigh,
@@ -45,11 +46,25 @@ impl WFAligner {
                 MemoryModel::MemoryMed => wfa2::wavefront_memory_t_wavefront_memory_med,
                 MemoryModel::MemoryLow => wfa2::wavefront_memory_t_wavefront_memory_low,
             };
+            attrib.alignment_scope = match alignment_scope {
+                AlignmentScope::Score => wfa2::alignment_scope_t_compute_score,
+                AlignmentScope::Alignment => wfa2::alignment_scope_t_compute_alignment,
+            };
             attrib
         };
         Self {
             attributes,
             inner: std::ptr::null_mut(),
+        }
+    }
+}
+
+impl Drop for WFAligner {
+    fn drop(&mut self) {
+        unsafe {
+            if !self.inner.is_null() {
+                wfa2::wavefront_aligner_delete(self.inner);
+            }
         }
     }
 }
@@ -78,11 +93,17 @@ impl WFAlignerGapAffine {
     }
 }
 
-pub trait WFAlign {
+pub trait Align {
     fn align_end_to_end(&mut self, pattern: &[u8], text: &[u8]) -> AlignmentStatus;
+
+    fn alignment_score(&self) -> i32;
+
+    fn alignment_cigar(&self) -> String;
+
+    fn alignment_matching(&self, pattern: &[u8], text: &[u8]) -> (String, String, String);
 }
 
-impl WFAlign for WFAlignerGapAffine {
+impl Align for WFAlignerGapAffine {
     fn align_end_to_end(&mut self, pattern: &[u8], text: &[u8]) -> AlignmentStatus {
         unsafe {
             // Configure
@@ -98,15 +119,52 @@ impl WFAlign for WFAlignerGapAffine {
             AlignmentStatus::from_i32(status)
         }
     }
-}
 
-impl Drop for WFAligner {
-    fn drop(&mut self) {
+    fn alignment_score(&self) -> i32 {
+        unsafe { (*self.aligner.inner).cigar.score }
+    }
+
+    fn alignment_cigar(&self) -> String {
         unsafe {
-            if !self.inner.is_null() {
-                wfa2::wavefront_aligner_delete(self.inner);
+            let begin_offset = (*self.aligner.inner).cigar.begin_offset;
+            let cigar_operations = (*self.aligner.inner)
+                .cigar
+                .operations
+                .offset(begin_offset as isize) as *const u8;
+            let cigar_length = ((*self.aligner.inner).cigar.end_offset - begin_offset) as usize;
+            let cigar_str = unsafe { slice::from_raw_parts(cigar_operations, cigar_length) };
+            String::from_utf8_lossy(cigar_str).to_string()
+        }
+    }
+
+    fn alignment_matching(&self, pattern: &[u8], text: &[u8]) -> (String, String, String) {
+        let cigar = self.alignment_cigar();
+        let mut pattern_iter = pattern.iter();
+        let mut text_iter = text.iter();
+        let mut pattern_match = String::new();
+        let mut middle_match = String::new();
+        let mut text_match = String::new();
+        for c in cigar.chars() {
+            match c {
+                'M' | 'X' => {
+                    pattern_match.push(*pattern_iter.next().unwrap() as char);
+                    middle_match.push('|');
+                    text_match.push(*text_iter.next().unwrap() as char);
+                }
+                'D' => {
+                    pattern_match.push(*pattern_iter.next().unwrap() as char);
+                    middle_match.push(' ');
+                    text_match.push('-');
+                }
+                'I' => {
+                    pattern_match.push('-');
+                    middle_match.push(' ');
+                    text_match.push(*text_iter.next().unwrap() as char);
+                }
+                _ => panic!("Unknown cigar operation: {}", c),
             }
         }
+        (pattern_match, middle_match, text_match)
     }
 }
 
@@ -122,5 +180,15 @@ mod tests {
         let text = b"TCTATACTGCGCGTTTGGAGAAATAAAATAGT";
         let status = aligner.align_end_to_end(pattern, text);
         assert_eq!(status, AlignmentStatus::StatusSuccessful);
+        assert_eq!(aligner.alignment_score(), -24);
+        assert_eq!(
+            aligner.alignment_cigar(),
+            "MMMXMMMMDMMMMMMMIMMMMMMMMMXMMMMMM"
+        );
+        let (a, b, c) = aligner.alignment_matching(pattern, text);
+        assert_eq!(
+            format!("{}\n{}\n{}", a, b, c),
+            "TCTTTACTCGCGCGTT-GGAGAAATACAATAGT\n|||||||| ||||||| ||||||||||||||||\nTCTATACT-GCGCGTTTGGAGAAATAAAATAGT"
+        );
     }
 }
